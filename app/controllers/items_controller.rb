2ac6a7225c71e8b1375b5431ba207ffe7438d244
Said Hussein <saidhussein12342@gmail.com>
2022-10-01 22:57:39 +0300

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
-    render json: items, include: :user
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create 
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private 
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found
+    render json: { error: "Item not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { error: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
